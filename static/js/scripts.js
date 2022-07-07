14082a05cad5dfea4713fb7f342d5d34678cfa8b
Rogelio Rodriguez <rogelio23roriguez@gmail.com>
2022-07-07 09:05:36 -0300

    base html css

---
+++ static/js/scripts.js
@@ -0,0 +1,4 @@
+let balanceValue = document.getElementById('balanceValue'), incomeValue = document.getElementById('incomeValue'), expensesValue = document.getElementById('expensesValue'), form = document.getElementsByTagName('form') ;
+
+
+
